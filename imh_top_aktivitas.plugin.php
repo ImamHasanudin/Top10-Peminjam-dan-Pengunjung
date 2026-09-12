1c97d96ff037932dcf13a97f61862b55e5b97819
ImamHasanudin <116447667+ImamHasanudin@users.noreply.github.com>
2026-09-12 14:36:53 +0700

    Update imh_top_aktivitas.plugin.php

--- imh_top_aktivitas.plugin.php
+++ imh_top_aktivitas.plugin.php
@@ -5,7 +5,7 @@
  * Description: 3 laporan terpisah - 10 Peminjam Tersering, 10 Pengunjung
  *              Tersering, dan 10 Pengunjung Teraktif (Peminjaman + Kunjungan
  *              terbanyak) - masing-masing dengan filter rentang tanggal.
- * Version: 2.0.0
+ * Version: 1.0.0
  * Author: Imam Hasanudin
  * Author URI: https://github.com/ImamHasanudin/
  * Compatibility: SLiMS 9 (Bulian) ke atas | PHP 7.4 dan PHP 8.0 ke atas
